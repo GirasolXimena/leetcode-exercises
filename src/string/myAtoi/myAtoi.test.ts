1e5106e6667cde9c737b4a034ced8a5ae4a572c7
Ximena Girasol <girasolximena@gmail.com>
2024-11-12 03:23:27 +0000

    start string exercises (myAtoi)

---
+++ src/string/myAtoi/myAtoi.test.ts
@@ -0,0 +1,91 @@
+import { describe, expect, it, test } from 'vitest'
+import { myAtoi } from './myAtoi'
+
+describe('myAtoi', () => {
+    describe('ignores any leading whitespace', () => {
+        test('it ignores leading spaces', () => {
+            let s = '         123'
+            let result = myAtoi(s)
+            let whitespaceRegex = new RegExp(/\s/)
+            expect(whitespaceRegex.test(String(result))).toBeFalsy()
+            expect(result).toBe(123)
+        })
+        test('it ignores other types of whitespace', () => {
+            let s = `\n\t\n123`
+            let result = myAtoi(s)
+            let whitespaceRegex = new RegExp(/\s/)
+            expect(whitespaceRegex.test(String(result))).toBeFalsy()
+            expect(result).toBe(123)
+        })
+    })
+
+    describe('determines sign by checking if next character is - or +', () => {
+
+        test('it reads a +', () => {
+            let s = '+042'
+            let result = myAtoi(s)
+            expect(result).toBe(42)
+        })
+        test('it reads a -', () => {
+            let s = '-042'
+            let result = myAtoi(s)
+            expect(result).toBe(-42)
+        })
+    })
+
+    describe('it strips leading zeroes', () => {
+        test('reads integer by skipping leading zeros until a non-digit character is encountered', () => {
+            let s = '00000000thello'
+            let result = myAtoi(s)
+            expect(result).toBe(0)
+        })
+        test('reads integer by skipping leading zeros until the end of the string is encountered', () => {
+            let s = '0000000000000'
+            let result = myAtoi(s)
+            expect(result).toBe(0)
+        })
+        test('0-1', () => {
+            let s = '0-1'
+            let result = myAtoi(s)
+            expect(result).toBe(0)
+        })
+    })
+
+    describe('rounds if the integer is out of 32-bit signed integer range', () => {
+        const MAX_INTEGER = (2 ** 31)
+        const MIN_INTEGER = -(2 ** 31) - 1
+        test('positive big number gets rounded down to 2^31', () => {
+            let s = '42234234235145145643623456456450'
+            let result = myAtoi(s)
+            expect(result).toBeLessThanOrEqual(MAX_INTEGER)
+        })
+        test('negative big number gets rounded up to -2^31 - 1', () => {
+            let s = '-423423423423453451454154320'
+            let result = myAtoi(s)
+            expect(result).toBeGreaterThanOrEqual(MIN_INTEGER)
+        })
+        test('negative big number gets rounded up to -2^31 - 1', () => {
+            let s = "21474836460"
+            let result = myAtoi(s)
+            expect(result).toBe(2147483647)
+        })
+    })
+
+    describe('leetcode test examples', () => {
+        test('example 2', () => {
+            let s = ' -042'
+            let result = myAtoi(s)
+            expect(result).toBe(-42)
+        })
+        test('example 2', () => {
+            let s = ' -042'
+            let result = myAtoi(s)
+            expect(result).toBe(-42)
+        })
+        test('example 3', () => {
+            let s = '1337c0d3'
+            let result = myAtoi(s)
+            expect(result).toBe(1337)
+        })
+    })
+})
---
+++ src/string/myAtoi/myAtoi.ts
@@ -0,0 +1,56 @@
+const clamp = (num: number, min: number, max: number): number => {
+    if (num <= min) return min
+    if (num >= max) return max
+    return num 
+}
+
+const MAX_INTEGER = (2 ** 31) - 1
+const MIN_INTEGER = -(2 ** 31)
+
+const clamp32Bit = (n: number) => clamp(n, MIN_INTEGER, MAX_INTEGER)
+
+const removeWhitespace = (s: string): string => {
+    let i = 0
+    while(i < s.length && /\s/.test(s[i])) {
+        i++
+    }
+    return i === 0 ? s : s.substring(i)
+}
+
+const checkIfPositive = (s: string): boolean => {
+    return s[0] !== '-'
+}
+
+const removeLeadingZeroes = (s: string): string => {
+    let i = 0
+    if(s[0] === '+' || s[0] === '-') i = 1
+    while(i < s.length && s[i] === '0') {
+        i++
+    }
+
+    return i === 0 ? s : s.substring(i)
+}
+
+const extractInteger = (s: string): string => {
+    let i = 0
+    while(i < s.length && /[0-9]/.test(s[i])) {
+        i++
+    }
+    
+    return i === 0 ? '0' : s.substring(0, i)
+}
+
+const convertToNumber =(s: string): number => {
+    let result = Number(s)
+    if(isNaN(result)) return 0
+    return result
+}
+
+export const myAtoi = (s: string): number => {
+    s = removeWhitespace(s)
+    const isPositive = checkIfPositive(s)
+    s = removeLeadingZeroes(s)
+    s = extractInteger(s)
+    let num = convertToNumber(s)
+    return clamp32Bit(isPositive ? num : -num)
+}
