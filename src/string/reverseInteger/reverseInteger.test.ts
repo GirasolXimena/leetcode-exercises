2c94b9b9048f18959d0c9fd10e3062a90d7acc69
Ximena Girasol <girasolximena@gmail.lcom>
2024-12-29 02:20:57 +0000

    more test fixes

--- src/string/reverseInteger/reverseInteger.test.ts
+++ src/string/reverseInteger/reverseInteger.test.ts
@@ -16,25 +16,19 @@ const MAX_INTEGER = (2 ** 31)
 const MIN_INTEGER = -(2 ** 31) - 1
 
 describe('reverse integer', () => {
-  it('returns 0 if value is not a number',() => {
-    let result = reverseInteger('not a number')
-    expect(result).toBe(0)
-  })
   describe('returns 0 if value is outside 32-bit integer range', () => {
     test('greater than 2^31', () => {
-      let result =reverseInteger(`${MAX_INTEGER}`)
-      expect(result).toBe(MAX_INTEGER);
-      result =reverseInteger(`${MAX_INTEGER + 1}`)
-      expect(result).toBe(0);
+      let result =reverseInteger(2147483641)
+      expect(result).toBe(1463847412);
     })
     test('less than -2^31 - 1', () => {
-      let result = reverseInteger(`${MIN_INTEGER}`)
+      let result = reverseInteger(MIN_INTEGER)
       expect(result).toBe(MIN_INTEGER)
-      result = reverseInteger(`${MIN_INTEGER - 1}`)
+      result = reverseInteger(MIN_INTEGER - 1)
       expect(result).toBe(0)
     })
   })
-  test('return num', () => {
-    expect(reverseInteger('123')).toBe(123)
+  describe('leetcode test cases', () => {
+    expect(reverseInteger(123)).toBe(321)
   })
 })
--- src/string/reverseInteger/reverseInteger.ts
+++ src/string/reverseInteger/reverseInteger.ts
@@ -1,7 +1,32 @@
-export const reverseInteger = (s: string) => {
-  let num = Number(s);
-  if(isNaN(num)) num = 0;
-  if(num > (2 ** 31)) num = 0;
-  if(num < -(2 ** 31) - 1) num = 0;
-  return num;
+// 2,147,483,647  (start from 0)
+//  214,748,364
+// -2,147,483,648 (start from -1)
+
+// -214,748,364
+
+
+export function reverseInteger(x: number): number {
+  let reverseInt = 0
+  let pop = 0
+  let num = Math.abs(x)
+
+  while (num !== 0) {
+      pop = num % 10
+
+      num = (num - pop) / 10
+
+      if (reverseInt > (2 ** 31) / 10 || (reverseInt === (2 ** 31) / 10 && pop > 7)) {
+          return 0
+
+      }
+      if (reverseInt < -(2 ** 31) / 10 || (reverseInt === -(2 ** 31) / 10 && pop < -8)) {
+          return 0
+      }
+
+      reverseInt = reverseInt * 10 + pop
+  }
+
+
+  return x > 0 ? reverseInt : -reverseInt
 }
+
