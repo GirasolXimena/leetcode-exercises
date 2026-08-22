afc2a1f4af529e3dc5ddc79c32cb0c8197769ed0
Ximena Girasol <girasol.ximena@gmail.com>
2026-08-22 19:17:07 +0000

    fix tests for reverseInt

--- src/string/reverseInteger/reverseInteger.test.ts
+++ src/string/reverseInteger/reverseInteger.test.ts
@@ -11,24 +11,33 @@
 import { describe, expect, it, test } from "vitest"
 import { reverseInteger } from "./reverseInteger"
 
-
-const MAX_INTEGER = (2 ** 31)
-const MIN_INTEGER = -(2 ** 31) - 1
+const MAX_INTEGER = 2 ** 31 - 1
+const MIN_INTEGER = -(2 ** 31)
 
 describe('reverse integer', () => {
   describe('returns 0 if value is outside 32-bit integer range', () => {
     test('greater than 2^31', () => {
-      let result =reverseInteger(2147483641)
-      expect(result).toBe(1463847412);
+      let result =reverseInteger(MAX_INTEGER )
+      expect(result).toBe(0);
     })
     test('less than -2^31 - 1', () => {
       let result = reverseInteger(MIN_INTEGER)
-      expect(result).toBe(MIN_INTEGER)
-      result = reverseInteger(MIN_INTEGER - 1)
       expect(result).toBe(0)
     })
   })
+  describe('returns reversed value if value is within 32-bit integer range', () => {
+    test('within 2^31', () => {
+    let result =reverseInteger(2147483631)
+      expect(result).toBe(1363847412);
+    })
+    test('within -2^31 - 1', () => {
+      let result = reverseInteger(-2147483641)
+      expect(result).toBe(-1463847412)
+    })
+  })
   describe('leetcode test cases', () => {
-    expect(reverseInteger(123)).toBe(321)
+    test('example test case 1', () => {
+      expect(reverseInteger(123)).toBe(321)
+    })
   })
 })
